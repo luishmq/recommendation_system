22f5c26b5fe7f214ba5abf405617133fedf42eb8
“Luis” <mrqueiroz22014@gmail.com>
2023-09-29 11:14:09 -0300

    Start to create Streamlit page

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Sistema de Recomendação de Livros",
+    page_icon="📚",
+    layout='wide'
+)
+
+st.title("Bem-vindo(a) ao painel do Projeto de Sistema de Recomendação de Livros! 📚")
+
+st.markdown(
+    """
+     Um sistema de recomendação é uma classe de aplicativos de software e técnicas que fornecem sugestões ou recomendações personalizadas aos usuários. Essas recomendações podem ser sobre uma variedade de itens, como produtos, serviços, conteúdo digital (filmes, músicas, artigos), amigos ou qualquer outra coisa que possa ser recomendada com base nos interesses e preferências dos usuários. A principal finalidade de um sistema de recomendação é ajudar os usuários a encontrar itens relevantes e interessantes em meio a uma vasta quantidade de opções disponíveis.
+
+    """
+)
